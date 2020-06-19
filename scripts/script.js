e43fb365f54585aeb42c7af8c1e3e322347cbbd3
Greta Neverauskaite <kika0125@gmail.com>
2020-06-19 22:56:05 +0300

    added background, on easy way with pshop

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,40 @@ console.log('Dont run in circles')
 
 //little engine for divs apearing in htmls, by random
 // this is a test comment
+// hide one page in sourceCode
 
 var pagesCollected;
-var timeUntilCaught
+var timeUntilCaught = 0;
+var pageAppearsInThisPath; //boolean
+var theGameHasStarted = false; //boolean
+
+
+
+
+function newGame() {
+    console.log('the game has started')
+    pagesCollected = 0;
+    theGameHasStarted = true;
+    //choose difficulty
+    //redirect to new game enviroment
+
+}
+function loadGame() {
+    console.log('this should load a game')
+    theGameHasStarted = true;
+}
+function help() {
+    console.log('this will show a modal with instructions')
+}
+
+if (timeUntilCaught <= 0) {
+    gameOver();
+};
+
+function gameOver() {
+    //show how many mages collected
+    //how highscore? (how fast pages found and how many?)
+    //show main menu or allow to redirect to main menu
+    console.log('game over')
+    theGameHasStarted = false;
+};
